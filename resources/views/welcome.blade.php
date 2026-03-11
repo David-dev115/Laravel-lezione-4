d71ba60422cb4b121d9e7169642568a43f726af8
David Paparo <d.paparo@tiscali.it>
2026-03-11 14:59:45 +0100

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<x-layout>
+
+    <div class="container-fluid">
+        <div class="row justify-contente-center" >
+            <div class="col-12">
+                <h1 class="text-center display-4" >
+                    CIAO
+                </h1>
+            </div>
+        </div>
+
+    </div>
+
+</x-layout>
+
